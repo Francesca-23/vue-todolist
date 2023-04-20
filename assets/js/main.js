e266ea0e09e067c3988b4751fe175c1249584a02
Francesca-23 <francescadidomenico4@gmail.com>
2023-04-20 14:56:17 +0200

    creazione della lista dinamica

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+/*
+MILESTONE 1
+Stampare all'interno di una lista HTML un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+*/
+
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+        
+        todos: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            }
+        ],
+
+    }
+  },
+  methods: {
+
+    eliminate(i){
+        this.todos.splice(i, 1)
+    }
+
+  }
+}).mount('#app')
